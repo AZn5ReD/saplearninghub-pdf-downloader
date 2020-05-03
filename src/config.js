8d0be35e372cfe9352609fa5bca984875b54a237
AZn5ReD <1781837+AZn5ReD@users.noreply.github.com>
2020-05-04 01:51:26 +0200

    Fix export config

--- src/config.js
+++ src/config.js
@@ -4,16 +4,18 @@ import minimist from "minimist";
 const argv = minimist(process.argv.slice(2));
 const configFile =
   process.env.NODE_ENV === "test" ? "./config_test.json" : "./config.json";
+let config = {};
 
 try {
-  const config = JSON.parse(fs.readFileSync(configFile));
-  export default {
-    DOWNLOAD_URL: argv.url ? argv.url : config.DOWNLOAD_URL,
-    LOGIN: argv.login ? argv.login : config.LOGIN,
-    PASSWORD: argv.password ? argv.password : config.PASSWORD,
-    TARGET_DIR: argv.target ? argv.target : config.TARGET_DIR,
-    DEBUG: argv.debug ? argv.debug : config.DEBUG,
-  };
+  config = JSON.parse(fs.readFileSync(configFile));
 } catch (error) {
   console.error("Error while reading config file:", error);
 }
+
+export default {
+  DOWNLOAD_URL: argv.url ? argv.url : config.DOWNLOAD_URL,
+  LOGIN: argv.login ? argv.login : config.LOGIN,
+  PASSWORD: argv.password ? argv.password : config.PASSWORD,
+  TARGET_DIR: argv.target ? argv.target : config.TARGET_DIR,
+  DEBUG: argv.debug ? argv.debug : config.DEBUG,
+};
